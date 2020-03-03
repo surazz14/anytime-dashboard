c37cda6679ff5fd3a1ba4a8157fe27814159ffa8
suraj <surajdhakal427@gmail.com>
2020-03-03 23:20:18 +0545

    Updates

---
+++ precache-manifest.530b22e233ff31e438699dbb80852a6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc94c0040bfee4031826d7b78a02ad85",
+    "url": "/surazz14/anytime-dashboard/index.html"
+  },
+  {
+    "revision": "66e4cf5b5cdebc90de60",
+    "url": "/surazz14/anytime-dashboard/static/css/2.26fe57cc.chunk.css"
+  },
+  {
+    "revision": "c312bd90eec30c7e0a99",
+    "url": "/surazz14/anytime-dashboard/static/css/main.62893fdd.chunk.css"
+  },
+  {
+    "revision": "66e4cf5b5cdebc90de60",
+    "url": "/surazz14/anytime-dashboard/static/js/2.47919d1e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/surazz14/anytime-dashboard/static/js/2.47919d1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c312bd90eec30c7e0a99",
+    "url": "/surazz14/anytime-dashboard/static/js/main.1e29884e.chunk.js"
+  },
+  {
+    "revision": "76b28a5e259003985f90",
+    "url": "/surazz14/anytime-dashboard/static/js/runtime-main.c388872b.js"
+  }
+]);
